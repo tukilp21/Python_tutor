70b6f9df784fe59d985658e2804ce73ccd8d0d51
Bill <plee0032@student.monash.edu>
2023-12-16 11:39:23 +1100

    lesson 16 Dec

--- Part 2 - Loops/loop1.py
+++ Part 2 - Loops/loop1.py
@@ -18,31 +18,58 @@ All type of loops:
 
 
 # NOTE: If you just want to use literally just LOOP FUNCTION - how many time the code is being repeated
-num_loop = 5
+# num_loop = 5
 
 # print(list(range(num_loop)))
 
 # for item in range(num_loop):
 #     print(f"This sentence will be print {num_loop} time")
 
+'''
+Display all the element (item) in a list
+Format:
+    
+    # 1. meat
+    # 2. apple
+    # 3. banana
+    # 4. mouse
+'''
+def print_list(target_list):
+    for item in target_list:
+        # print("hello: ")
+        ''' Recommend method'''
+        item_index = target_list.index(item) + 1  # Plus one because index of a list start at 0
+        # Retur index of the specified item in the list
+        print(f"{item_index}. {item}")
+
+        '''This is a straightforward (manual) method'''
+        # item_index = item_index + 1
+
+
+def print_list_v2(target_list):
+    for i in range(len(target_list)):
+        item = target_list[i]
+        item_idx = target_list.index(item) + 1
+        print(f"{item_idx}. {item}")
+
+
+def print_list_v3(target_list):
+    for idx, item in enumerate(target_list):
+        print(f"{idx + 1}. {item}")
+
+
+
 #################################################################################
 # NOTE: If the list has meaningful values - use can "reuse" it
 
-shopping_list = ["meat", "apple", "banana", "mouse"]
+# shopping_list = ["meat", "apple", "banana", "mouse"]
 
-# item_index = shopping_list.index("apple")
-item_index = 1
+# # print_list_v2(shopping_list)
+# print_list_v3(shopping_list)
 
-for item in shopping_list:
-    # print("hello: ")
-    # Retur index of the specified item in the list
-    print(f"{item_index}. {item}")
-    item_index = item_index + 1
+# # add element
+# shopping_list.append("car")
 
-# 1. meat
-# 2. apple
-# 3. banana
-# 4. mouse
-
-
+# print(len(shopping_list))
+# print_list(shopping_list)
 
